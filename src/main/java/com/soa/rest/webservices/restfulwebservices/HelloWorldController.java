63ed8549dd8ddfa673db3e0b74021509fb4f1d9d
Junior Loshima <junior.okangondoloshima@uvs.edu.sn>
2020-12-19 16:55:28 +0000

    Step 06 - Enhancing the Hello World Service with a Path Variable

--- src/main/java/com/soa/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/soa/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.soa.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,13 @@ public class HelloWorldController {
 	{
 		
 	return new helloWorldBean("Hello World");
+	
+	}
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public helloWorldBean helloWorldPathVariable(@PathVariable String name)
+	{
+		
+	return new helloWorldBean(String.format("Hello World, %s",name));
 	
 	}
 
