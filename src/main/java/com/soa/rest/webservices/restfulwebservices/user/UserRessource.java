677b0a6c2a0806dd0154ac377aba8cc21218fcd3
Junior Loshima <junior.okangondoloshima@uvs.edu.sn>
2020-12-19 20:24:52 +0000

    Step 10 - Enhancing POST Method to return correct HTTP Status Code and Location

--- src/main/java/com/soa/rest/webservices/restfulwebservices/user/UserRessource.java
+++ src/main/java/com/soa/rest/webservices/restfulwebservices/user/UserRessource.java
@@ -1,13 +1,16 @@
 package com.soa.rest.webservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserRessource {
@@ -27,9 +30,12 @@ public class UserRessource {
 	}
 	
 	@PostMapping("/users")
-	public void createdUser(@RequestBody User user)
+	public ResponseEntity<Object> createdUser(@RequestBody User user)
 	{
 		User savedUser = service.save(user);
+		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+		return ResponseEntity.created(location).build();
 	}
 
 }
