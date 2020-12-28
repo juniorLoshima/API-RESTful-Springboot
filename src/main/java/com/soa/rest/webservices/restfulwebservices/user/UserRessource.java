fdfec2fa58decd1959eff6a9f1606b062d7a955d
Junior Loshima <junior.okangondoloshima@uvs.edu.sn>
2020-12-28 13:23:29 +0000

    Step 14 - Implementing DELETE Method to delete a User Resource

--- src/main/java/com/soa/rest/webservices/restfulwebservices/user/UserRessource.java
+++ src/main/java/com/soa/rest/webservices/restfulwebservices/user/UserRessource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,6 +40,14 @@ public class UserRessource {
 		
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
 		return ResponseEntity.created(location).build();
+	}
+	
+	@DeleteMapping ("/users/{id}")
+	public void deleteUser(@PathVariable int id)
+	{
+		User user = service.deleteById(id);
+		if(user==null)
+			throw new UserNotFoundException("id-"+ id);
 	}
 
 }
